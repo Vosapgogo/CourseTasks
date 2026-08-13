97fa4d88541bc997e4dfe5f6fae6a7a510ff0183
Vosapgogo <saha10t2007@gmail.com>
2026-08-13 19:53:40 +0200

    fix: handle edge case when coefficient a is zero

--- src/com/shpp/p2p/cs/oTyshchenko/assigment2/Assigment2Part1.java
+++ src/com/shpp/p2p/cs/oTyshchenko/assigment2/Assigment2Part1.java
@@ -14,16 +14,22 @@ public class Assigment2Part1 {
         System.out.print("Please enter c: ");
         double c = scanner.nextDouble();
 
-        double discriminant = (b * b) - (4 * a * c);
-        if (discriminant > 0) {
-            double sqrtDiscriminant = Math.sqrt(discriminant);
-            double firstRoot = (-b + sqrtDiscriminant)/(2 * a);
-            double secondRoot = (-b - sqrtDiscriminant)/(2 * a);
-            System.out.printf("There are two roots: %.1f and %.1f\n", firstRoot, secondRoot);
-        } else if (discriminant == 0) {
-            double onlyRoot = -b/(2 * a);
-            System.out.printf("There is one root: %.1f\n", onlyRoot);
-        } else System.out.println("There are no real roots");
+        if (a==0) {
+            System.out.println("This is not a quadratic equation (a cannot be 0)");
+        } else {
+            double discriminant = (b * b) - (4 * a * c);
+
+            if (discriminant > 0) {
+                double sqrtDiscriminant = Math.sqrt(discriminant);
+                double firstRoot = (-b + sqrtDiscriminant) / (2 * a);
+                double secondRoot = (-b - sqrtDiscriminant) / (2 * a);
+                System.out.printf("There are two roots: %.1f and %.1f\n", firstRoot, secondRoot);
+            } else if (discriminant == 0) {
+                double onlyRoot = -b / (2 * a);
+
+                System.out.printf("There is one root: %.1f\n", onlyRoot);
+            } else System.out.println("There are no real roots");
+        }
 
         scanner.close();
     }
