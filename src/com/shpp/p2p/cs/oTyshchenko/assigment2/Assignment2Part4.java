6f91a477b71f1d462be5031619ab2cd120b754ed
Vosapgogo <saha10t2007@gmail.com>
2026-08-13 20:01:37 +0200

    refactor: extract drawRectangle method and add color constants

--- src/com/shpp/p2p/cs/oTyshchenko/assigment2/Assignment2Part4.java
+++ src/com/shpp/p2p/cs/oTyshchenko/assigment2/Assignment2Part4.java
@@ -13,29 +13,27 @@ public class Assignment2Part4 extends WindowProgram {
     public static final double FLAG_WIDTH = 200;
     public static final double FLAG_HEIGHT = 170;
 
+    public static final Color BLUE = Color.BLUE;
+    public static final Color WHITE = new Color(251, 251,248);
+    public static final Color RED = Color.RED;
+
     public void run() {
-        GRect rectangle1 = new GRect((getWidth()-FLAG_WIDTH)/2, (getHeight()-FLAG_HEIGHT)/2, FLAG_WIDTH/3 ,FLAG_HEIGHT);
-        rectangle1.setColor(Color.BLUE);
-        rectangle1.setFilled(true);
-        rectangle1.setFillColor(Color.BLUE);
-        add(rectangle1);
-
-        GRect rectangle2 = new GRect((getWidth()-FLAG_WIDTH)/2+FLAG_WIDTH/3, (getHeight()-FLAG_HEIGHT)/2, FLAG_WIDTH/3 ,FLAG_HEIGHT);
-        rectangle2.setColor(new Color(251, 251,248));
-        rectangle2.setFilled(true);
-        rectangle2.setFillColor(new Color(251, 251,248));
-        add(rectangle2);
-
-        GRect rectangle3 = new GRect((getWidth()-FLAG_WIDTH)/2+2*FLAG_WIDTH/3, (getHeight()-FLAG_HEIGHT)/2, FLAG_WIDTH/3 ,FLAG_HEIGHT);
-        rectangle3.setColor(Color.RED);
-        rectangle3.setFilled(true);
-        rectangle3.setFillColor(Color.RED);
-        add(rectangle3);
+        drawRectangle((getWidth()-FLAG_WIDTH)/2, BLUE);
+        drawRectangle((getWidth()-FLAG_WIDTH)/2+FLAG_WIDTH/3, WHITE);
+        drawRectangle((getWidth()-FLAG_WIDTH)/2+2*FLAG_WIDTH/3, RED);
 
         GLabel l = new GLabel("Flag of France");
         l.setFont("Times New Roman-18");
         l.setColor(Color.BLACK);
         l.setLocation(getWidth() - l.getWidth(), getHeight() - l.getDescent());
         add(l);
+    }
+
+    public void drawRectangle(double x, Color color) {
+        GRect rectangle = new GRect(x,(getHeight()-FLAG_HEIGHT)/2, FLAG_WIDTH/3 ,FLAG_HEIGHT);
+        rectangle.setColor(color);
+        rectangle.setFilled(true);
+        rectangle.setFillColor(color);
+        add(rectangle);
     }
 }
