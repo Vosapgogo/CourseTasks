1b90e67696b3d95d370c0f5d1a2be05d1b7e954d
Vosapgogo <saha10t2007@gmail.com>
2026-09-10 23:37:17 +0200

    feat: add Racket class for paddle generation

---
+++ src/com/shpp/p2p/cs/oTyshchenko/assignment4/Racket.java
@@ -0,0 +1,17 @@
+package com.shpp.p2p.cs.oTyshchenko.assignment4;
+
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+
+import java.awt.*;
+
+public class Racket {
+
+    public GRect drawRectangle(double x, double y, double width, double height) {
+        GRect rectangle = new GRect(x, y, width, height);
+        rectangle.setColor(Color.BLACK);
+        rectangle.setFilled(true);
+        rectangle.setFillColor(Color.BLACK);
+        return rectangle;
+    }
+}
