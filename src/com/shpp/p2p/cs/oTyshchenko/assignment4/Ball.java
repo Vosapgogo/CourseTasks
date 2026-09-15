62edc7e1f5a1679d439d007175c56f54910f017d
Vosapgogo <saha10t2007@gmail.com>
2026-09-15 14:14:49 +0200

    refactor: enhance Ball class physics and structure
    - Added MAX_VY constant to limit falling speed and prevent tunneling.
    - Extracted wall collision checks into dedicated methods (hitsSideWall, hitsTopWall).
    - Added helper methods (isBelowBottom, getGraphics, bounceVertically, bounceHorizontally) for better interaction with the Game class.
    - Updated bounceOffRacket logic for safer upward trajectory.

--- src/com/shpp/p2p/cs/oTyshchenko/assignment4/Ball.java
+++ src/com/shpp/p2p/cs/oTyshchenko/assignment4/Ball.java
@@ -12,6 +12,7 @@ public class Ball {
 
     private static final double Y_VELOCITY = 3.0;
     private static final double ACCELERATION = 0.04;
+    private static final double MAX_VY = 7.0;
 
     public Ball() {
         vy = Y_VELOCITY;
@@ -39,26 +40,50 @@ public class Ball {
         return ballGraphics.getY();
     }
 
+    private boolean hitsSideWall(double screenWidth, int frameMargin) {
+        return ballGraphics.getX() <= frameMargin
+                || ballGraphics.getX() + ballGraphics.getWidth() >= screenWidth - frameMargin;
+    }
+
+    private boolean hitsTopWall(int frameMargin) {
+        return ballGraphics.getY() <= frameMargin;
+    }
+
     public void move(double screenWidth, int frameMargin) {
         if (vy > 0) {
-            vy += ACCELERATION;
+            vy = Math.min(vy + ACCELERATION, MAX_VY);
         }
 
         ballGraphics.move(vx, vy);
 
-        if (ballGraphics.getX() <= frameMargin || ballGraphics.getX() + ballGraphics.getWidth() >= screenWidth - frameMargin) {
-            vx = -vx;
+        if (hitsSideWall(screenWidth, frameMargin)) {
+            bounceHorizontally();
         }
-
-        if (ballGraphics.getY() <= frameMargin) {
-            vy = -vy;
+        if (hitsTopWall(frameMargin)) {
+            bounceVertically();
         }
     }
 
     public void bounceOffRacket(double racketY) {
-        vy = -Y_VELOCITY;
+        vy = -Math.abs(vy);
 
         double ballNewY = racketY - ballGraphics.getHeight();
         ballGraphics.setLocation(ballGraphics.getX(), ballNewY);
+    }
+
+    public void bounceVertically() {
+        vy = -vy;
+    }
+
+    public void bounceHorizontally() {
+        vx = -vx;
+    }
+
+    public boolean isBelowBottom(double screenHeight, int frameMargin) {
+        return ballGraphics.getY() >= screenHeight - frameMargin;
+    }
+
+    public GOval getGraphics() {
+        return ballGraphics;
     }
 }
