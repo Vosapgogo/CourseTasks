5a025f2c8baef38f2138b392ce6d16c3f1619936
Vosapgogo <saha10t2007@gmail.com>
2026-09-15 18:07:17 +0200

    Add "Play again" restart flow, wire up sub-stepped collisions
    - Wrap the whole game in an outer while(true) loop in run(): after a win/loss, show a "Play again" button and block until it's clicked, then clear the board and start a brand-new game
    - Extract bricksLeft to a field (was a local in playGame()) so it can be reset each restart
    - Add drawRestartButton() and the button/text constants (BUTTON_*, WIN_TEXT, GAME_OVER_TEXT, FINISH_FONT) backing it
    - Replace the single ball.move() + overlapsRacket() check with moveBallWithCollisions(), which now uses Ball's new sub-stepped movement (getStepsForThisFrame()/ moveOneStep()) and per-step collision detection via getCollidingObject(Ball)
    - mousePressed now also checks for a click on the restart button before handling racket dragging
    - Remove unused gameOver/loseMessage fields

--- src/com/shpp/p2p/cs/oTyshchenko/assignment4/Game.java
+++ src/com/shpp/p2p/cs/oTyshchenko/assignment4/Game.java
@@ -20,6 +20,18 @@ public class Game extends WindowProgram {
     /** Offset of the paddle up from the bottom */
     private static final int PADDLE_Y_OFFSET = 30;
 
+    /** Dimension, label and styling for the "Play again" button shown after each game ends */
+    private static final int BUTTON_WIDTH = 150;
+    private static final int BUTTON_HEIGHT = 40;
+
+    private static final String BUTTON_TEXT = "PLAY AGAIN";
+    private static final String BUTTON_FONT = "SansSerif-bold-16";
+
+    /** Label and styling for the win / game-over banner */
+    private static final String WIN_TEXT = "YOU WIN!";
+    private static final String GAME_OVER_TEXT = "GAME OVER";
+    private static final String FINISH_FONT ="SansSerif-bold-36";
+
     /** Number of bricks per row */
     private static final int NBRICKS_PER_ROW = 10;
 
@@ -44,58 +56,79 @@ public class Game extends WindowProgram {
     public static final int FRAME_MARGIN = 10;
 
     private GRect racketGraphics;
-
     private GRect frameGraphics;
 
-    private double vx, vy;
+    private Robot robot;
 
     private boolean isDraggingRacket = false;
 
     private int livesLeft;
-    private boolean gameOver = false;
+    private int bricksLeft;
 
-    private GLabel loseMessage;
+    private volatile boolean restartClicked = false;
     private GRect restartButton;
     private GLabel restartLabel;
-
-    private Robot robot;
 
     public void run() {
         try {
             robot = new Robot();
-            drawFrame();
-
-            Racket racket = new Racket();
-            double paddleX = (getWidth() - PADDLE_WIDTH) / 2.0;
-            double paddleY = getHeight() - PADDLE_HEIGHT - PADDLE_Y_OFFSET;
-
-            racketGraphics = racket.drawRectangle(paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
-            add(racketGraphics);
-
-            Ball ball = new Ball();
-            double ballX = (getWidth() - BALL_RADIUS * 2) / 2.0;
-            double ballY = getHeight() / 2.0;
-
-            add(ball.drawCircle(ballX, ballY, BALL_RADIUS * 2));
-
-            Bricks bricks = new Bricks();
-            int availableWidth = getWidth() - 2 * FRAME_MARGIN - 2;
-            int brickWidth = (availableWidth - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
-
-            bricks.wallBuilding(this, getWidth(), brickWidth, BRICK_HEIGHT, BRICK_SEP, NBRICK_ROWS, NBRICKS_PER_ROW, BRICK_Y_OFFSET);
             addMouseListeners();
 
-            playGame(ball);
+            while (true) {
+                restartButton = null;
+                restartLabel = null;
+                removeAll();
+
+                drawFrame();
+
+                Racket racket = new Racket();
+                double paddleX = (getWidth() - PADDLE_WIDTH) / 2.0;
+                double paddleY = getHeight() - PADDLE_HEIGHT - PADDLE_Y_OFFSET;
+                racketGraphics = racket.drawRectangle(paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT);
+                add(racketGraphics);
+
+                Ball ball = new Ball();
+                double ballX = (getWidth() - BALL_RADIUS * 2) / 2.0;
+                double ballY = getHeight() / 2.0;
+                add(ball.drawCircle(ballX, ballY, BALL_RADIUS * 2));
+
+                Bricks bricks = new Bricks();
+                int availableWidth = getWidth() - 2 * FRAME_MARGIN - 2;
+                int brickWidth = (availableWidth - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+                bricks.wallBuilding(this, getWidth(), brickWidth, BRICK_HEIGHT, BRICK_SEP, NBRICK_ROWS, NBRICKS_PER_ROW, BRICK_Y_OFFSET);
+
+                playGame(ball);
+
+                drawRestartButton();
+
+                restartClicked = false;
+                while (!restartClicked) {
+                    pause(20);
+                }
+            }
         } catch (AWTException e) {
             e.printStackTrace();
         }
-
     }
 
     public void drawFrame() {
         frameGraphics = new GRect(FRAME_MARGIN, FRAME_MARGIN, getWidth()-2*FRAME_MARGIN, getHeight()-2*FRAME_MARGIN);
         frameGraphics.setColor(Color.BLACK);
         add(frameGraphics);
+    }
+
+    private void drawRestartButton() {
+        restartButton = new GRect((getWidth() - BUTTON_WIDTH) / 2.0, getHeight() / 2.0 + BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT - FRAME_MARGIN);
+        restartButton.setFilled(true);
+        restartButton.setColor(Color.LIGHT_GRAY);
+        add(restartButton);
+
+        restartLabel = new GLabel(BUTTON_TEXT);
+        restartLabel.setFont(BUTTON_FONT);
+
+        double labelX = restartButton.getX() + (restartButton.getWidth() - restartLabel.getWidth()) / 2.0;
+        double labelY = restartButton.getY() + restartButton.getHeight() / 2.0 + restartLabel.getAscent() / 2.0 - 2;
+        add(restartLabel, labelX, labelY);
     }
 
     private boolean overlapsRacket(Ball ball) {
@@ -114,24 +147,13 @@ public class Game extends WindowProgram {
     }
 
     private void playGame(Ball ball) {
-        int bricksLeft = NBRICK_ROWS * NBRICKS_PER_ROW;
+        bricksLeft = NBRICK_ROWS * NBRICKS_PER_ROW;
         livesLeft = NTURNS;
 
         waitForClick();
 
         while (livesLeft > 0 && bricksLeft > 0) {
-            ball.move(getWidth(), FRAME_MARGIN);
-
-            if (overlapsRacket(ball)) {
-                ball.bounceOffRacket(racketGraphics.getY());
-            } else {
-                GObject collider = getCollidingObject(ball.getX(), ball.getY());
-                if (collider != null && collider != frameGraphics && collider != racketGraphics) {
-                    remove(collider);
-                    ball.bounceVertically();
-                    bricksLeft--;
-                }
-            }
+            moveBallWithCollisions(ball);
 
             if (ball.isBelowBottom(getHeight(), FRAME_MARGIN)) {
                 livesLeft--;
@@ -139,7 +161,6 @@ public class Game extends WindowProgram {
                     double ballX = (getWidth() - BALL_RADIUS * 2) / 2.0;
                     double ballY = getHeight() / 2.0;
                     ball.getGraphics().setLocation(ballX, ballY);
-
                     waitForClick();
                 }
             }
@@ -150,10 +171,28 @@ public class Game extends WindowProgram {
         displayGameOverMessage(bricksLeft == 0);
     }
 
+    private void moveBallWithCollisions(Ball ball) {
+        int steps = ball.getStepsForThisFrame();
+        for (int i = 0; i < steps; i++) {
+            ball.moveOneStep(getWidth(), FRAME_MARGIN, steps);
+
+            GObject collider = getCollidingObject(ball);
+
+            if (collider == racketGraphics) {
+                ball.bounceOffRacket(racketGraphics.getY());
+            } else if (collider != null) {
+                ball.bounceOffBrick(collider);
+                remove(collider);
+                bricksLeft--;
+                break;
+            }
+        }
+    }
+
     private void displayGameOverMessage(boolean won) {
-        String text = won ? "YOU WIN!" : "GAME OVER";
+        String text = won ? WIN_TEXT : GAME_OVER_TEXT;
         GLabel message = new GLabel(text);
-        message.setFont("SansSerif-bold-36");
+        message.setFont(FINISH_FONT);
         message.setColor(won ? Color.GREEN : Color.RED);
 
         double x = (getWidth() - message.getWidth()) / 2.0;
@@ -161,36 +200,48 @@ public class Game extends WindowProgram {
         add(message, x, y);
     }
 
-    private GObject getCollidingObject(double ballX, double ballY) {
+    private GObject getCollidingObject(Ball ball) {
+        double x = ball.getX();
+        double y = ball.getY();
         double d = BALL_RADIUS * 2;
 
-        GObject collider = getElementAt(ballX, ballY);
-        if (collider != null) return collider;
+        GObject collider;
 
-        collider = getElementAt(ballX + d, ballY);
-        if (collider != null) return collider;
+        collider = getElementAt(x, y);
+        if (collider != null && collider != frameGraphics && collider != ball.getGraphics()) return collider;
 
-        collider = getElementAt(ballX, ballY + d);
-        if (collider != null) return collider;
+        collider = getElementAt(x + d, y);
+        if (collider != null && collider != frameGraphics && collider != ball.getGraphics()) return collider;
 
-        collider = getElementAt(ballX + d, ballY + d);
-        if (collider != null) return collider;
+        collider = getElementAt(x, y + d);
+        if (collider != null && collider != frameGraphics && collider != ball.getGraphics()) return collider;
+
+        collider = getElementAt(x + d, y + d);
+        if (collider != null && collider != frameGraphics && collider != ball.getGraphics()) return collider;
 
         return null;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        isDraggingRacket = true;
+        GObject obj = getElementAt(e.getX(), e.getY());
 
-        double centerX = racketGraphics.getX() + PADDLE_WIDTH / 2.0;
-        double centerY = racketGraphics.getY() + PADDLE_HEIGHT / 2.0;
+        if (restartButton != null && (obj == restartButton || obj == restartLabel)) {
+            restartClicked = true;
+            return;
+        }
 
-        if (robot != null) {
-            Point componentLocation = e.getComponent().getLocationOnScreen();
-            int screenX = (int) (componentLocation.x + centerX);
-            int screenY = (int) (componentLocation.y + centerY);
-            robot.mouseMove(screenX, screenY);
+        if (obj == racketGraphics) {
+            isDraggingRacket = true;
+            double centerX = racketGraphics.getX() + PADDLE_WIDTH / 2.0;
+            double centerY = racketGraphics.getY() + PADDLE_HEIGHT / 2.0;
+
+            if (robot != null) {
+                Point componentLocation = e.getComponent().getLocationOnScreen();
+                int screenX = (int) (componentLocation.x + centerX);
+                int screenY = (int) (componentLocation.y + centerY);
+                robot.mouseMove(screenX, screenY);
+            }
         }
     }
 
